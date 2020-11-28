1ca91dc464aede38f10564a970a89aff9181a19f
Lama AlDaej <46524571+LamaAlDaej@users.noreply.github.com>
2020-11-28 04:58:46 +0300

    Adhere PEP8

--- models.py
+++ models.py
@@ -12,6 +12,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -19,69 +21,75 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
 '''
 Movies
 '''
-class Movie(db.Model):  
-  __tablename__ = 'movies'
 
-  id = Column(db.Integer, primary_key=True)
-  title = Column(db.String, nullable=False)
-  release_date = Column(db.Date, nullable=False)
 
-  def __init__(self, title, release_date):
-    self.title = title
-    self.release_date = release_date
+class Movie(db.Model):
+    __tablename__ = 'movies'
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    id = Column(db.Integer, primary_key=True)
+    title = Column(db.String, nullable=False)
+    release_date = Column(db.Date, nullable=False)
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
 
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'release_date': self.release_date,
-    }
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+        }
+
 
 '''
 Actors
 '''
-class Actor(db.Model):  
-  __tablename__ = 'actors'
 
-  id = Column(db.Integer, primary_key=True)
-  name = Column(db.String, nullable=False)
-  age = Column(db.Integer, nullable=False)
-  gender = Column(db.String, nullable=False)
 
-  def __init__(self, name, age, gender):
-    self.name = name
-    self.age = age
-    self.gender = gender
+class Actor(db.Model):
+    __tablename__ = 'actors'
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    id = Column(db.Integer, primary_key=True)
+    name = Column(db.String, nullable=False)
+    age = Column(db.Integer, nullable=False)
+    gender = Column(db.String, nullable=False)
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit() 
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'age': self.age,
-      'gender': self.gender
-    }
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
